565fd9719d1625d002e01c28a9420088d7a75e49
Залим Лампежев <sabolch@yandex.ru>
2020-03-06 08:16:53 +0300

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+def fibonacci(x):
+
+    sequence = []
+    result = 0
+    n = 1
+
+    for i in range(x):
+        result = result + n
+        n = result - n
+        sequence.append(result)
+
+    golden_ratio = sequence[-1] / sequence[-2]
+
+    print(sequence)
+    print(golden_ratio)
+
+
+fibonacci(14)
